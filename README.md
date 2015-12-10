ed0a572eba2b48c60022d73040e923601c089f41
nju33 <jun.sasaki@mvrck.co.jp>
2015-12-10 18:33:29 +0900

    commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# postcss environment nju33
+
+```
+git clone git@github.com:totora0155/postcss-environment-nju33.git
+cd postcss-environment-nju33
+npm install
+npm start
+```
---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const fs = require('fs'),
+      gulp = require('gulp'),
+      watch = require('gulp-watch'),
+      sourcemaps = require('gulp-sourcemaps'),
+      postcss = require('gulp-postcss');
+
+const processors = [
+  require('stylelint'),
+  require('postcss-reporter')({clearMessages: true}),
+  require('postcss-cson-cssvars'),
+  require('postcss-color-function'),
+  require('postcss-assets'),
+  require('postcss-property-lookup'),
+  require('postcss-animation'),
+  require('postcss-easings'),
+  require('lost'),
+  require('postcss-selector-not'),
+  require('postcss-preref'),
+  require('postcss-namespace')({token: '__'}),
+  require('doiuse')({browsers: ['last 2 versions']}),
+  require('autoprefixer')({browser: ['last 2 versions']}),
+  require('css-mqpacker'),
+  // require('cssnano') 見づらくなるので今回はコメントアウト
+]
+
+gulp.task('postcss', () => {
+  gulp.src('./style.css')
+    .pipe(watch('./style.css'))
+    .pipe(sourcemaps.init())
+    .pipe(postcss(processors))
+    .pipe(sourcemaps.write('.'))
+  .pipe(gulp.dest('dist/'));
+});
